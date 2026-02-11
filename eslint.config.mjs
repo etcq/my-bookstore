b4451346fd9678479d228fcda1ca1aa7007df553
Anton Sushnikov <resonans93@gmail.com>
2026-02-11 16:37:57 +0500

    Init/project setup (#1)
    * init: added PR template, project description in README.md
    
    * init: install nexjs, configure eslint and prettier
    
    * init: add git hooks

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import {defineConfig, globalIgnores} from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import eslint from "@eslint/js";
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier/recommended';
+
+const eslintConfig = defineConfig([
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    extends: [
+      ...nextVitals,
+      eslint.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      prettier,
+    ],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      }
+    },
+    rules: {
+      "@typescript-eslint/consistent-type-definitions": ['error', 'interface'],
+      '@typescript-eslint/consistent-type-imports': 'error',
+    }
+  },
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+export default eslintConfig;
