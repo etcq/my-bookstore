df92d0dc5b65b44a28770565dd9b143293865c6d
etcq <resonans93@gmail.com>
2026-02-11 17:23:04 +0500

    feat: add header component, install UI library

---
+++ src/widgets/header/ui/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <div className="absolute top-0 left-0 w-full h-screen bg-white border-b border-gray-200">
+      This is the header component
+    </div>
+  );
+};
