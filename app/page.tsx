628e233ac43564b21eeba3cc93b3d9ab8abb4eb3
etcq <resonans93@gmail.com>
2026-02-11 17:58:03 +0500

    feat: added theme switcher

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { HomePage } from '@/pages/home';
+
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Hello</h1>
-    </div>
-  );
+  return <HomePage />;
 }
--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { ThemeSwitcher } from '@/features/theme-switcher';
 
 export const Header = () => {
   return (
-    <div className="absolute top-0 left-0 w-full h-screen bg-white border-b border-gray-200">
+    <div className="border-b-2">
       This is the header component
+      <ThemeSwitcher />
     </div>
   );
 };
