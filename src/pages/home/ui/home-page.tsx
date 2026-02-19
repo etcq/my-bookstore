ed89c53ea25b866f200a5e1a66a1bc3fd434753a
etcq <resonans93@gmail.com>
2026-02-19 19:45:17 +0500

    feat: add home page landing

--- src/pages/home/ui/home-page.tsx
+++ src/pages/home/ui/home-page.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import { PromoSlider } from './promo-slider';
+import { NewBooks } from '@/pages/home/ui/new-books-section';
+import { GenresSection } from '@/pages/home/ui/genres-section';
+import { FeaturesSection } from '@/pages/home/ui/features-section';
+import { NewsletterSection } from '@/pages/home/ui/newsletter-section';
+import { StatsSection } from './stats-section';
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="mx-auto">
+      <StatsSection />
+      <PromoSlider />
+      <NewBooks />
+      <GenresSection />
+      <FeaturesSection />
+      <NewsletterSection />
+    </div>
+  );
 };
