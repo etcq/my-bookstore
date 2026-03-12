df0c5614a2b014661b9ec4f97af2dd4eb0e79700
etcq <resonans93@gmail.com>
2026-03-12 11:53:18 +0500

    feat: implement book parameters and enhance cover image upload in create book form

--- src/features/create-book/ui/create-book-form.tsx
+++ src/features/create-book/ui/create-book-form.tsx
@@ -1,12 +1,14 @@
 'use client';
 
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { bookSchema, type TBookForm } from '@/entities/book/model/book.schema';
 import { FormControlledInput } from '@/shared/ui';
 import { Button } from '@/shared/ui/kit/button';
-import { bookParameters } from '@/entities/book/model/book-parametres';
+import { bookParameters } from '@/entities/book/model/book-parameters';
 import { createBook } from '@/features/create-book/api/create-book';
+import { uploadCover } from '@/features/create-book/api/upload-cover';
 import { CoverImageSelect } from '@/features/create-book/ui/cover-image-select';
 
 export const CreateBookForm = () => {
@@ -14,9 +16,22 @@ export const CreateBookForm = () => {
     resolver: zodResolver(bookSchema),
   });
 
+  const [coverFile, setCoverFile] = useState<File | null>(null);
+
   const onSubmit = async (data: TBookForm) => {
     try {
-      await createBook(data);
+      let coverPath: string | null = null;
+
+      if (coverFile) {
+        const file = coverFile;
+        coverPath = `private/${file.name}`;
+        await uploadCover(coverPath, file);
+      }
+
+      await createBook({
+        ...data,
+        cover: coverPath ?? undefined,
+      });
     } catch (error) {
       console.error('Error creating book:', error);
     }
@@ -37,7 +52,14 @@ export const CreateBookForm = () => {
             type={field.type}
           />
         ))}
-        <CoverImageSelect name="cover" label="Cover" control={control} />
+        <CoverImageSelect
+          name="cover"
+          label="Cover"
+          control={control}
+          onFileSelect={(file) => {
+            setCoverFile(file);
+          }}
+        />
 
         <Button type="submit" className="mt-3">
           Create
