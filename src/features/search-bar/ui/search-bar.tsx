cdc19e0e4925b728541864ac8374a69908a91c15
etcq <resonans93@gmail.com>
2026-03-26 20:33:14 +0500

    refactor: implement controlled input for the search bar and add a dedicated search button

--- src/features/search-bar/ui/search-bar.tsx
+++ src/features/search-bar/ui/search-bar.tsx
@@ -1,17 +1,18 @@
 'use client';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { Input } from '@/shared/ui/kit/input';
-import { type KeyboardEvent } from 'react';
+import { type KeyboardEvent, useState } from 'react';
+import { Button } from '@/shared/ui/kit/button';
+import { Search } from 'lucide-react';
 
 export const SearchBar = () => {
   const params = useSearchParams();
   const searchedString = params?.get('search');
   const pathname = usePathname();
   const router = useRouter();
+  const [value, setValue] = useState(searchedString ?? '');
 
-  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
-    if (event.key !== 'Enter') return;
-    const value = event.currentTarget.value;
+  const updateSearch = () => {
     if (value === searchedString || !pathname) return;
     const newParams = new URLSearchParams(params?.toString() ?? '');
     if (value) {
@@ -21,12 +22,30 @@ export const SearchBar = () => {
     }
     router.push(`${pathname}?${newParams}`);
   };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key !== 'Enter') return;
+    updateSearch();
+  };
+
   return (
-    <Input
-      type="text"
-      className="w-2/3 my-4 text-4xl"
-      defaultValue={searchedString ?? ''}
-      onKeyDown={handleKeyDown}
-    />
+    <div className="relative w-2/3">
+      <Input
+        type="text"
+        className="w-full my-4 md:text-xl"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+        onKeyDown={handleKeyDown}
+      />
+      <Button
+        size="icon"
+        className="absolute right-0 top-4"
+        onClick={updateSearch}
+      >
+        <Search />
+      </Button>
+    </div>
   );
 };
