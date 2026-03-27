08600df26fb446935938ffb6297719e2495a2aa6
Anton Sushnikov <resonans93@gmail.com>
2026-03-27 18:25:07 +0500

    Feat/search (#9)
    * feat: add search bar to the catalog page and enable book filtering by title or author
    
    * refactor: implement controlled input for the search bar and add a dedicated search button
    
    * feat: add placeholder text to the search bar input field
    
    * feat: add `checkParams` utility to normalize URL parameter types and apply it in CatalogPage
    
    * fix: fix search bar state update

---
+++ src/features/search-bar/ui/search-bar.tsx
@@ -0,0 +1,51 @@
+'use client';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { Input } from '@/shared/ui/kit/input';
+import { type KeyboardEvent, useState } from 'react';
+import { Button } from '@/shared/ui/kit/button';
+import { Search } from 'lucide-react';
+
+export const SearchBar = () => {
+  const params = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+  const [value, setValue] = useState(params?.get('search') ?? '');
+
+  const updateSearch = () => {
+    if (value === params?.get('search') || !pathname) return;
+    const newParams = new URLSearchParams(params?.toString() ?? '');
+    if (value) {
+      newParams.set('search', value);
+    } else {
+      newParams.delete('search');
+    }
+    router.push(`${pathname}?${newParams}`);
+  };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key !== 'Enter') return;
+    updateSearch();
+  };
+
+  return (
+    <div className="relative w-2/3">
+      <Input
+        type="text"
+        className="w-full my-4 md:text-xl"
+        value={value}
+        placeholder="Search books or authors..."
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+        onKeyDown={handleKeyDown}
+      />
+      <Button
+        size="icon"
+        className="absolute right-0 top-4"
+        onClick={updateSearch}
+      >
+        <Search />
+      </Button>
+    </div>
+  );
+};
--- src/pages/catalog/ui/catalog.tsx
+++ src/pages/catalog/ui/catalog.tsx
@@ -1,13 +1,22 @@
 import { BookList } from '@/widgets/book-list';
 import { Suspense } from 'react';
 import { Loading } from '@/shared/ui/loading';
+import { SearchBar } from '@/features/search-bar';
+import { checkParams } from '@/shared/lib/check-params';
 
-export const CatalogPage = () => {
+interface ICatalogPageProps {
+  searchParams: Promise<Record<string, string | string[] | undefined>>;
+}
+
+export async function CatalogPage({ searchParams }: ICatalogPageProps) {
+  const params = await searchParams;
+  const searchingString = checkParams(params.search);
   return (
-    <div className="flex h-full w-full flex-col">
+    <div className="flex h-full w-full flex-col items-center">
+      <SearchBar key={searchingString} />
       <Suspense fallback={<Loading />}>
-        <BookList className="w-[70%]" />
+        <BookList className="w-[70%]" searchedString={searchingString} />
       </Suspense>
     </div>
   );
-};
+}
